06bcda19f13cbdf2f118fdbb274d0ba9d137ab9e
Mohamed Belkamel <belkamelmohamed@gmail.com>
2020-06-25 22:21:27 +0100

    PUBLISHING TO GITHUB

--- controllers/main.js
+++ controllers/main.js
@@ -33,7 +33,7 @@ exports.index = (req, ress) => {
                     }
                 }
             }
-            element.label = !index ? "Total Corona cases": index === 1 ? "Total deceased" : "Total recovered"
+            element.label = !index ? "Total Corona cases": index === 1 ? "Total deceased" : "Total recovered "
             
             data.push(element)
         }
